be54a4d53f27d6260c5cf14c5f723cf2467c2d93
Redeix <brayden.j.m.ford@gmail.com>
2026-03-01 23:52:21 -0600

    glass lens artisan fix

--- src/main/java/su/terrafirmagreg/core/common/data/recipes/ArtisanType.java
+++ src/main/java/su/terrafirmagreg/core/common/data/recipes/ArtisanType.java
@@ -14,15 +14,18 @@ import com.gregtechceu.gtceu.common.data.GTItems;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTSoundEntries;
 import com.gregtechceu.gtceu.data.recipe.CustomTags;
+import com.simibubi.create.AllTags;
 
 import net.dries007.tfc.client.TFCSounds;
 import net.dries007.tfc.common.items.Powder;
 import net.dries007.tfc.common.items.TFCItems;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import lombok.Getter;
@@ -34,13 +37,58 @@ import su.terrafirmagreg.core.TFGCore;
  */
 public class ArtisanType {
 
+    /**
+     * Wrapper class for ingredients that can be either an ItemStack or a TagKey<Item>.
+     */
+    public static class Ingredient {
+        @Nullable
+        private final ItemStack itemStack;
+        @Nullable
+        private final TagKey<Item> tag;
+
+        private Ingredient(@Nullable ItemStack itemStack, @Nullable TagKey<Item> tag) {
+            if ((itemStack == null) == (tag == null)) {
+                throw new IllegalArgumentException("Ingredient must have exactly one of ItemStack or Tag");
+            }
+            this.itemStack = itemStack;
+            this.tag = tag;
+        }
+
+        public static Ingredient of(ItemStack itemStack) {
+            return new Ingredient(itemStack, null);
+        }
+
+        public static Ingredient of(TagKey<Item> tag) {
+            return new Ingredient(null, tag);
+        }
+
+        public boolean isItemStack() {
+            return itemStack != null;
+        }
+
+        public boolean isTag() {
+            return tag != null;
+        }
+
+        @Nullable
+        public ItemStack getItemStack() {
+            return itemStack;
+        }
+
+        @Nullable
+        public TagKey<Item> getTag() {
+            return tag;
+        }
+    }
+
     @Getter
     private final ResourceLocation id;
     @Getter
-    private final ArrayList<ItemStack> inputItems;
+    private final ArrayList<Ingredient> inputIngredients;
     @Getter
-    private final ArrayList<TagKey<Item>> toolTags;
+    private final ArrayList<Ingredient> toolRequirements;
     @Getter
+    @Nullable
     private final ResourceLocation activeTexture;
     @Getter
     @Nullable
@@ -61,22 +109,24 @@ public class ArtisanType {
      * Constructs a new ArtisanType.
      *
      * @param name The name for this recipe type.
-     * @param inputItemA The primary input item.
-     * @param inputItemB The secondary input item (nullable).
-     * @param toolA The primary tool tag required.
-     * @param toolB The secondary tool tag required.
-     * @param activeTexture The texture when active.
+     * @param inputA The primary input.
+     * @param inputB The secondary input (nullable).
+     * @param toolA The primary tool required.
+     * @param toolB The secondary tool required.
+     * @param activeTexture The texture when active (nullable).
      * @param inactiveTexture The texture when inactive (nullable).
      * @param clickSound The sound event played on click.
      * @param borderTexture The border texture (nullable).
      * @param clickVolume The volume of the click sound.
      * @param clickPitch The pitch of the click sound.
      */
-    public ArtisanType(String name, ItemStack inputItemA, @Nullable ItemStack inputItemB, TagKey<Item> toolA, TagKey<Item> toolB, ResourceLocation activeTexture,
-            @Nullable ResourceLocation inactiveTexture, SoundEvent clickSound, @Nullable ResourceLocation borderTexture, float clickVolume, float clickPitch) {
+    public ArtisanType(String name, Ingredient inputA, @Nullable Ingredient inputB,
+            Ingredient toolA, Ingredient toolB, @Nullable ResourceLocation activeTexture,
+            @Nullable ResourceLocation inactiveTexture, SoundEvent clickSound,
+            @Nullable ResourceLocation borderTexture, float clickVolume, float clickPitch) {
         this.id = TFGCore.id(name);
-        inputItems = new ArrayList<>(Stream.of(inputItemA, inputItemB).filter(Objects::nonNull).toList());
-        toolTags = new ArrayList<>(Arrays.asList(toolA, toolB));
+        this.inputIngredients = new ArrayList<>(Stream.of(inputA, inputB).filter(Objects::nonNull).toList());
+        this.toolRequirements = new ArrayList<>(Arrays.asList(toolA, toolB));
         this.activeTexture = activeTexture;
         this.inactiveTexture = inactiveTexture;
         this.clickSound = clickSound;
@@ -100,10 +150,10 @@ public class ArtisanType {
 
     public static final ArtisanType CASTING_MOLD = new ArtisanType(
             "casting_mold",
-            GTItems.SHAPE_EMPTY.get().getDefaultInstance(),
-            ItemStack.EMPTY,
-            CustomTags.HAMMERS,
-            CustomTags.MALLETS,
+            Ingredient.of(GTItems.SHAPE_EMPTY.get().getDefaultInstance()),
+            null,
+            Ingredient.of(CustomTags.HAMMERS),
+            Ingredient.of(CustomTags.MALLETS),
             textureLocation("casting_mold_active"),
             textureLocation("casting_mold_inactive"),
             TFCSounds.ANVIL_HIT.get(),
@@ -112,10 +162,10 @@ public class ArtisanType {
             2.0f);
     public static final ArtisanType EXTRUDER_MOLD = new ArtisanType(
             "extruder_mold",
-            GTItems.SHAPE_EMPTY.get().getDefaultInstance(),
-            ItemStack.EMPTY,
-            CustomTags.WIRE_CUTTERS,
-            CustomTags.FILES,
+            Ingredient.of(GTItems.SHAPE_EMPTY.get().getDefaultInstance()),
+            null,
+            Ingredient.of(CustomTags.WIRE_CUTTERS),
+            Ingredient.of(CustomTags.FILES),
             textureLocation("extruder_mold_active"),
             null,
             GTSoundEntries.WIRECUTTER_TOOL.getMainEvent(),
@@ -124,10 +174,10 @@ public class ArtisanType {
             0.7f);
     public static final ArtisanType RESIN_BOARD = new ArtisanType(
             "resin_board",
-            GTItems.COATED_BOARD.get().getDefaultInstance(),
-            new ItemStack(ChemicalHelper.get(TagPrefix.wireGtSingle, GTMaterials.Copper).getItem(), 8),
-            CustomTags.SCREWDRIVERS,
-            CustomTags.WIRE_CUTTERS,
+            Ingredient.of(GTItems.COATED_BOARD.get().getDefaultInstance()),
+            Ingredient.of(new ItemStack(ChemicalHelper.get(TagPrefix.wireGtSingle, GTMaterials.Copper).getItem(), 8)),
+            Ingredient.of(CustomTags.SCREWDRIVERS),
+            Ingredient.of(CustomTags.WIRE_CUTTERS),
             textureLocation("blank_resin_board"),
             textureLocation("printed_resin_board"),
             GTSoundEntries.WRENCH_TOOL.getMainEvent(),
@@ -136,10 +186,10 @@ public class ArtisanType {
             2.0f);
     public static final ArtisanType RESIN_BOARD_FOUR = new ArtisanType(
             "resin_board_4x",
-            new ItemStack(GTItems.COATED_BOARD.get().getDefaultInstance().getItem(), 4),
-            new ItemStack(ChemicalHelper.get(TagPrefix.wireGtQuadruple, GTMaterials.Copper).getItem(), 8),
-            CustomTags.SCREWDRIVERS,
-            CustomTags.WIRE_CUTTERS,
+            Ingredient.of(new ItemStack(GTItems.COATED_BOARD.get().getDefaultInstance().getItem(), 4)),
+            Ingredient.of(new ItemStack(ChemicalHelper.get(TagPrefix.wireGtQuadruple, GTMaterials.Copper).getItem(), 8)),
+            Ingredient.of(CustomTags.SCREWDRIVERS),
+            Ingredient.of(CustomTags.WIRE_CUTTERS),
             textureLocation("blank_resin_board"),
             textureLocation("printed_resin_board"),
             GTSoundEntries.WRENCH_TOOL.getMainEvent(),
@@ -148,10 +198,10 @@ public class ArtisanType {
             2.0f);
     public static final ArtisanType PHENOLIC_BOARD = new ArtisanType(
             "phenolic_board",
-            GTItems.PHENOLIC_BOARD.get().getDefaultInstance(),
-            new ItemStack(ChemicalHelper.get(TagPrefix.wireGtSingle, GTMaterials.Silver).getItem(), 8),
-            CustomTags.SCREWDRIVERS,
-            CustomTags.WIRE_CUTTERS,
+            Ingredient.of(GTItems.PHENOLIC_BOARD.get().getDefaultInstance()),
+            Ingredient.of(new ItemStack(ChemicalHelper.get(TagPrefix.wireGtSingle, GTMaterials.Silver).getItem(), 8)),
+            Ingredient.of(CustomTags.SCREWDRIVERS),
+            Ingredient.of(CustomTags.WIRE_CUTTERS),
             textureLocation("blank_phenolic_board"),
             textureLocation("printed_phenolic_board"),
             GTSoundEntries.WRENCH_TOOL.getMainEvent(),
@@ -160,10 +210,10 @@ public class ArtisanType {
             2.0f);
     public static final ArtisanType PHENOLIC_BOARD_FOUR = new ArtisanType(
             "phenolic_board_4x",
-            new ItemStack(GTItems.PHENOLIC_BOARD.get().getDefaultInstance().getItem(), 4),
-            new ItemStack(ChemicalHelper.get(TagPrefix.wireGtQuadruple, GTMaterials.Silver).getItem(), 8),
-            CustomTags.SCREWDRIVERS,
-            CustomTags.WIRE_CUTTERS,
+            Ingredient.of(new ItemStack(GTItems.PHENOLIC_BOARD.get().getDefaultInstance().getItem(), 4)),
+            Ingredient.of(new ItemStack(ChemicalHelper.get(TagPrefix.wireGtQuadruple, GTMaterials.Silver).getItem(), 8)),
+            Ingredient.of(CustomTags.SCREWDRIVERS),
+            Ingredient.of(CustomTags.WIRE_CUTTERS),
             textureLocation("blank_phenolic_board"),
             textureLocation("printed_phenolic_board"),
             GTSoundEntries.WRENCH_TOOL.getMainEvent(),
@@ -173,15 +223,15 @@ public class ArtisanType {
 
     public static final ArtisanType GLASS_LENS = new ArtisanType(
             "optical_borosilicate",
-            new ItemStack(ForgeRegistries.ITEMS.getValue(TFGCore.id("optical_borosilicate_blank"))),
-            new ItemStack(TFCItems.POWDERS.get(Powder.FLUX).get(), 8),
-            CustomTags.BUZZSAWS,
-            CustomTags.MALLETS,
-            textureLocation("blank_phenolic_board"),
-            textureLocation("printed_phenolic_board"),
-            GTSoundEntries.CUT.getMainEvent(),
-            textureLocation("phenolic_board_border_4x"),
-            0.8f,
+            Ingredient.of(new ItemStack(Objects.requireNonNullElse(ForgeRegistries.ITEMS.getValue(TFGCore.id("optical_borosilicate_blank")), Items.AIR))),
+            Ingredient.of(new ItemStack(TFCItems.POWDERS.get(Powder.FLUX).get(), 8)),
+            Ingredient.of(AllTags.AllItemTags.SANDPAPER.tag),
+            Ingredient.of(new ItemStack(TFCItems.GEM_SAW.get(), 1)),
+            null,
+            textureLocation("glass_chipped"),
+            SoundEvents.GLASS_BREAK,
+            textureLocation("glass_border"),
+            0.5f,
             2.0f);
 
     /**
