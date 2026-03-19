33df34adee155f1563110cef3e83620f566fc623
TomPlop <tomdidome@gmail.com>
2026-03-19 14:32:45 -0400

    Balance boiler (#369)

--- src/main/java/su/terrafirmagreg/core/compat/emi/TFGEmiPlugin.java
+++ src/main/java/su/terrafirmagreg/core/compat/emi/TFGEmiPlugin.java
@@ -3,6 +3,7 @@ package su.terrafirmagreg.core.compat.emi;
 import java.util.Arrays;
 
 import com.forsteri.createliquidfuel.core.BurnerStomachHandler;
+import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTItems;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllTags;
@@ -20,6 +21,8 @@ import su.terrafirmagreg.core.TFGCore;
 import su.terrafirmagreg.core.common.data.TFGBlocks;
 import su.terrafirmagreg.core.common.data.TFGRecipeTypes;
 import su.terrafirmagreg.core.common.data.recipes.ArtisanRecipe;
+import su.terrafirmagreg.core.common.data.tfgt.machine.TFGMultiMachines;
+import su.terrafirmagreg.core.common.data.tfgt.machine.multiblock.electric.TFGLargeBoilerMachine;
 
 @EmiEntrypoint
 public class TFGEmiPlugin implements EmiPlugin {
@@ -35,6 +38,9 @@ public class TFGEmiPlugin implements EmiPlugin {
 
     public static final EmiRecipeCategory ARTISAN_TABLE = new EmiRecipeCategory(TFGCore.id("artisan_table"),
             EmiStack.of(TFGBlocks.ARTISAN_TABLE.get()));
+
+    public static final EmiRecipeCategory LARGE_BOILER_BOOSTER = new EmiRecipeCategory(TFGCore.id("large_boiler_booster"),
+            EmiStack.of(GTBlocks.FIREBOX_STEEL.asItem()));
 
     @Override
     public void register(EmiRegistry emiRegistry) {
@@ -72,6 +78,13 @@ public class TFGEmiPlugin implements EmiPlugin {
 
         emiRegistry.addCategory(BLOCK_INTERACTION);
         Arrays.stream(BlockInteractionInfo.RECIPES).forEach(emiRegistry::addRecipe);
+
+        emiRegistry.addCategory(LARGE_BOILER_BOOSTER);
+        emiRegistry.addWorkstation(LARGE_BOILER_BOOSTER,
+                EmiStack.of(TFGMultiMachines.LARGE_BOILER_BRONZE.getBlock().asItem()));
+        emiRegistry.addWorkstation(LARGE_BOILER_BOOSTER,
+                EmiStack.of(TFGMultiMachines.LARGE_STEEL_BOILER.getBlock().asItem()));
+        TFGLargeBoilerMachine.getBoosters().forEach(booster -> emiRegistry.addRecipe(new LargeBoilerBoosterRecipe(booster)));
 
         emiRegistry.addCategory(ARTISAN_TABLE);
         emiRegistry.addWorkstation(ARTISAN_TABLE, EmiStack.of(TFGBlocks.ARTISAN_TABLE.get().asItem()));
