de5997494a30e1da4105a2d2b419cbe6e7cdc7da
Pyritie <pyritie@gmail.com>
2026-03-24 16:18:48 +0000

    disable the helve mixin for now

--- src/main/java/su/terrafirmagreg/core/mixins/common/vintageimprovements/HelveBlockEntityMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/vintageimprovements/HelveBlockEntityMixin.java
@@ -2,10 +2,7 @@ package su.terrafirmagreg.core.mixins.common.vintageimprovements;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.negodya1.vintageimprovements.content.kinetics.helve_hammer.HelveBlockEntity;
 import com.simibubi.create.foundation.item.SmartInventory;
 
@@ -17,9 +14,10 @@ public class HelveBlockEntityMixin {
     // spotless:off
     /**
      * @author Ujhik
-     * @reason Fix helve hammer not processing output items like for example helve hammer processing iron boom into refined iron bloom but not this last into iron ingot without removing the refined ones and adding them again.
+     * @reason Fix helve hammer not processing output items like for example helve hammer processing iron bloom into
+	 * refined iron bloom but not this last into iron ingot without removing the refined ones and adding them again.
      */
-    @WrapOperation(
+    /*@WrapOperation(
             method = "tick",
             at = @At(
                     value = "INVOKE",
@@ -31,5 +29,5 @@ public class HelveBlockEntityMixin {
     // spotless:on
     private boolean tfg$checkBothInventories(SmartInventory instance, Operation<Boolean> original) {
         return original.call(instance) && outputInv.isEmpty();
-    }
+    }*/
 }
