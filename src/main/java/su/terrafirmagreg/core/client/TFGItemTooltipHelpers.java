454d1205eb438217c95b926bbf28cf271ec0358f
Pyritie <pyritie@gmail.com>
2026-03-29 13:12:14 +0100

    spotless

--- src/main/java/su/terrafirmagreg/core/client/TFGItemTooltipHelpers.java
+++ src/main/java/su/terrafirmagreg/core/client/TFGItemTooltipHelpers.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import org.jetbrains.annotations.NotNull;
+
 import net.dries007.tfc.util.Drinkable;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.Registries;
@@ -23,7 +25,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.Mod;
 
-import org.jetbrains.annotations.NotNull;
 import su.terrafirmagreg.core.TFGCore;
 import su.terrafirmagreg.core.common.capability.ILargeEgg;
 import su.terrafirmagreg.core.common.capability.LargeEggCapability;
