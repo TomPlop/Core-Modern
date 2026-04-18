832aacc831ace468ab3b4a05f088cafec3805d0a
Pyritie <pyritie@gmail.com>
2026-04-18 13:46:34 +0100

    stop loose rocks generating on sea ice

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/features/LooseRockFeatureMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/features/LooseRockFeatureMixin.java
@@ -1,21 +1,24 @@
 package su.terrafirmagreg.core.mixins.common.tfc.features;
 
-import net.dries007.tfc.common.blocks.TFCBlocks;
-import net.dries007.tfc.world.feature.LooseRockFeature;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.dries007.tfc.common.blocks.TFCBlocks;
+import net.dries007.tfc.world.feature.LooseRockFeature;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
 @Mixin(value = LooseRockFeature.class, remap = false)
 public class LooseRockFeatureMixin {
 
-	@Inject(method = "canGenerateOn", at = @At("HEAD"),	remap = false, cancellable = true)
-	private void tfg$canGenerateOn(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-		if (state == Blocks.ICE.defaultBlockState() || state == TFCBlocks.SEA_ICE.get().defaultBlockState()) {
-			cir.setReturnValue(false);
-		}
-	}
+    @Inject(method = "canGenerateOn", at = @At("HEAD"), remap = false, cancellable = true)
+    private void tfg$canGenerateOn(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+		// Ice and sea ice had their #minecraft:ice tag removed (for reasons related to being able to pick them up
+		// and break them to move water sources around), so add the checks for those items back
+        if (state == Blocks.ICE.defaultBlockState() || state == TFCBlocks.SEA_ICE.get().defaultBlockState()) {
+            cir.setReturnValue(false);
+        }
+    }
 }
