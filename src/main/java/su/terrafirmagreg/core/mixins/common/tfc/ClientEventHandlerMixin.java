8b8a62120a13acf7609d74c953a173c26e847e4e
Pyritie <pyritie@gmail.com>
2026-05-01 23:59:30 +0100

    thinking emoji

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/ClientEventHandlerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/ClientEventHandlerMixin.java
@@ -1,5 +1,6 @@
 package su.terrafirmagreg.core.mixins.common.tfc;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -12,8 +13,21 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import su.terrafirmagreg.core.common.entity.animals.tfcwolf.TFCWolfRenderer;
 
 @Mixin(value = ClientEventHandler.class, remap = false)
-public class ClientEvenHandlerMixin {
-    @ModifyArg(method = "registerEntityRenderers", remap = false, slice = @Slice(from = @At(value = "FIELD", target = "Lnet/dries007/tfc/common/entities/TFCEntities;WOLF:Lnet/minecraftforge/registries/RegistryObject;")), at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/event/EntityRenderersEvent$RegisterRenderers;registerEntityRenderer(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/client/renderer/entity/EntityRendererProvider;)V", ordinal = 0), index = 1)
+public class ClientEventHandlerMixin {
+    // spotless:off
+    @ModifyArg(method = "registerEntityRenderers",
+		remap = false,
+		slice = @Slice(
+			from = @At(
+				value = "FIELD",
+				target = "Lnet/dries007/tfc/common/entities/TFCEntities;WOLF:Lnet/minecraftforge/registries/RegistryObject;",
+				opcode = Opcodes.GETSTATIC)),
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraftforge/client/event/EntityRenderersEvent$RegisterRenderers;registerEntityRenderer(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/client/renderer/entity/EntityRendererProvider;)V",
+			ordinal = 0),
+		index = 1)
+	// spotless:on
     private static EntityRendererProvider<?> tfg$replaceWolfRenderer(EntityRendererProvider<?> original) {
         return (EntityRendererProvider<PackPredator>) TFCWolfRenderer::new;
     }
